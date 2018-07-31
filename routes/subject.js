35fb3328a5ca4034fbd3b23f2adcc822170b174b
rizariza69 <riza.riza69@gmail.com>
2018-07-31 08:42:58 +0700

    rz

--- routes/subject.js
+++ routes/subject.js
@@ -3,7 +3,7 @@ const routes = express.Router()
 const model = require('../models/')
 const Subject = model.Subject
 
-
+//tampilkan semua subject
 routes.get('/',function(req,res){
     Subject
     .findAll({
@@ -14,10 +14,60 @@ routes.get('/',function(req,res){
         // res.json(dataSubject)
     })
 } )
+// add Subject
 
-// routes.get('/', (req,res) => {
-//     res.send('SUBJECT')
-// })
+routes.get("/add", (req, res) => {
+    res.render("/addSubject.ejs");
+  });
+  
+  routes.post("/add", (req, res) => {
+    models.Subject.create(req.body)
+  
+      .then(() => {
+        res.redirect("/subject");
+      })
+      .catch(err => {
+        res.redirect("/subject", { show: err });
+      });
+  });
+  
+//edit (update) subject
+
+  routes.get("/subject/edit/:id", function(req, res) {
+    Subject.findById(req.params.id)
+    .then(function(subjects) {
+      res.render("/editSubject.ejs", { subjects: subjects });
+    });
+  });
+  
+  routes.post("/edit/:id", function(req, res) {
+    let id = req.params.id;
+    Subject.update(
+      {
+        subjectName: req.body.subjectName
+      },
+      { where: { id: id } 
+    })
+    .then(function() {
+      res.redirect("/subject");
+    });
+  });
+  
+
+//delete subject
+
+routes.get('/delete/:id', function(req,res){
+    let id = req.params.id
+    Subject.destroy({
+        where: {id:id} 
+    })
+        .then(function(){
+            res.redirect('/Subject')
+        })
+  
+})
+
+
 
 module.exports = routes
 
